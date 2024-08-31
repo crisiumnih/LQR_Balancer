4811c55fe4985bd9fc1a313ef800ec6f1c5e14ab
crisiumnih <fasfasmag@proton.me>
2024-08-31 15:21:35 +0530

    formatting the code

--- firmware/main/firmware.c
+++ firmware/main/firmware.c
@@ -5,12 +5,15 @@
 #include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "mpu.h"
 #include "sdkconfig.h"
 #include "stepper.h"
 #include <stdint.h>
 #include <stdio.h>
 
-static const char *TAG = "mpu_test";
+static const char *TAG = "State Variables: ";
+static const char *TAG3 = "System: ";
+static const char *TAG2 = "Frequencies provided: ";
 int direction_step = 0;
 #include "sra_board.h"
 
@@ -23,50 +26,57 @@ float step_to_rad = 0.0314;
 float phi_dot = 0.001;
 int base_max = 1200;
 int base_min = 25;
+float deg_to_rad=0.0175;
+
+/*state_eqn sets up the theta and theta_dot in state space variables*/
 
 void state_eqn(float err[]) {
     float euler_angle[2], mpu_offset[2] = {0.0f, 0.0f};
     if (enable_mpu6050() == ESP_OK) {
         if (read_mpu6050(euler_angle, mpu_offset) == ESP_OK) {
 
-            float current_1 = (euler_angle[1]) * 0.0175;
+            float current_angle_1 = (euler_angle[1]) * deg_to_rad;
 
             vTaskDelay(pdMS_TO_TICKS(10));
-            float current_2 = (euler_angle[1]) * 0.0175;
-            ESP_LOGI(TAG, "Roll: %0.2f | Pitch: %0.2f", euler_angle[0],
-                     euler_angle[1]);
+            float current_angle_2 = (euler_angle[1]) * deg_to_rad;
+            ESP_LOGI(TAG, "Pitch: %0.2f", euler_angle[1]);
 
-            err[0] = final_state[0] - current_2;
-            float d_theta1 = (current_2 - current_1) / 0.01;
+            err[0] = final_state[0] - current_angle_2;
+            float d_theta1 = (current_angle_2 - current_angle_1) / 0.01;
             err[1] = final_state[1] - d_theta1;
-            if (current_2 >= 0)
+            if (current_angle_2 >= 0)
                 direction_step = 1;
             else
                 direction_step = 0;
         }
     }
 
-    // float dd_theta = (d_theta2-d_theta1)/0.01;
-    // err[2]=final_state[2]-dd_theta;
 }
 
-float get_phid() {
+
+
+
+float get_phi_dot() {
     float angle1 = get_phi_angle();
     vTaskDelay(pdMS_TO_TICKS(30));
     float angle2 = get_phi_angle();
-    printf("\nphi:%f\n", angle2);
+    ESP_LOGI(TAG,"\nphi:%f\n", angle2);
     phi_dot = (angle2 - angle1) / 0.03;
     return phi_dot;
 }
+
+/*To obtain required feedback angular acceleration of motor*/
 
 float set_ang_acc(float theta, float theta_dot, float phi_dot) {
     return -(K[0] * theta + K[1] * theta_dot + K[2] * phi_dot);
 }
 
+
+
 void set_stepper_freq(float phi_ddot, float pitch) {
     int freq;
     if (phi_ddot == 0)
-        printf("problem");
+        ESP_LOGE(TAG3,"problem in freq");
 
     freq = phi_ddot * K_u;
     if (base_freq == 0) {
@@ -78,35 +88,33 @@ void set_stepper_freq(float phi_ddot, float pitch) {
         freq = base_min;
 
     base_freq = freq;
-    printf("base freq: %f\n", base_freq);
-    // function to setup freq of motor to the desired freq
-    set_stepper_frequency(freq, pitch);
-
     
+    // function to setup freq of motor to the desired freq
+    set_stepper_frequency(freq, pitch, direction_step);
+
+    ESP_LOGI(TAG2,"Base freq: %f\n", base_freq);
 }
 
-// feedback_acc is phi_ddot
+
 
 void control_loop() {
-    printf("control loop started");
+    ESP_LOGI(TAG,"Control loop started\n");
     float err[3];
     
     state_eqn(err);
     err[2] = phi_dot;
     float feedback_acc = set_ang_acc(err[0], err[1], err[2]);
-    printf("feedback accn: %f\n", feedback_acc);
+    ESP_LOGI(TAG2,"Feedback Accn: %f\n", feedback_acc);
 
     // set motor cw
     set_stepper_direction(direction_step);
 
-    if (feedback_acc == 0)
-        feedback_acc = 20;
     if (feedback_acc < 0) {
         feedback_acc = -(feedback_acc);
     }
 
     
-    phi_dot = get_phid();
+    phi_dot = get_phi_dot();
     float euler_angle[2], mpu_offset[2] = {0.0f, 0.0f};
     if (read_mpu6050(euler_angle, mpu_offset) == ESP_OK) {
         set_stepper_freq(feedback_acc, euler_angle[1]);
@@ -115,20 +123,19 @@ void control_loop() {
 
 void stepper_task(void *pvParameters) {
     init_stepper(NULL);
-    printf("motro ok\n");
+    ESP_LOGI(TAG,"MOTOR INITIALISED\n");
 
     while (1) {
         set_stepper_direction(direction_step);
-        set_stepper_frequency(base_freq, 1);
+        set_stepper_frequency(base_freq, 1, direction_step);
         vTaskDelay(10);
     }
 }
 
-void control_task(void *pvParameters) {
-    printf("control task initiated\n");
+void control_task(void *pvParameters){
 
     while (1) {
-        printf("control task loop\n");
+        
         vTaskDelay(10);
         control_loop();
         vTaskDelay(pdMS_TO_TICKS(10));
@@ -139,14 +146,14 @@ void app_main(void) {
 
     xTaskCreatePinnedToCore(&stepper_task, "stepper_task", 8192, NULL, 1, NULL,
                             0);
-    // printf("Stepper task created\n");
+    ESP_LOGI(TAG3,"Stepper task created\n");
 
     xTaskCreatePinnedToCore(&control_task, "control_task", 8192, NULL, 2, NULL,
                             1);
-    // printf("Control task created\n");
+    ESP_LOGI(TAG3,"Control task created\n");
 
     while (1) {
-        printf("Main loop running\n");
-        vTaskDelay(pdMS_TO_TICKS(10));
+        ESP_LOGI(TAG3,"Main loop running\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
--- firmware/main/stepper.c
+++ firmware/main/stepper.c
@@ -1,5 +1,6 @@
 #include "esp_rom_sys.h"
 #include "stepper.h"
+#include <stdio.h>
 #define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
 #include "esp_log.h"
 static const char *TAG = "stepper";
@@ -10,9 +11,8 @@ void init_stepper(void *arg) {
 
     ledc_timer_config_t ledc_timer = {.speed_mode = LEDC_HIGH_SPEED_MODE,
                                       .timer_num = LEDC_TIMER_0,
-                                      .duty_resolution =
-                                          LEDC_TIMER_12_BIT, // -bit
-                                      .freq_hz = 2000,       // 500Hz frequency
+                                      .duty_resolution = LEDC_TIMER_12_BIT, // -bit
+                                      .freq_hz = 2000,                      //  frequency
                                       .clk_cfg = LEDC_AUTO_CLK};
     ledc_timer_config(&ledc_timer);
 
@@ -26,8 +26,8 @@ void init_stepper(void *arg) {
     ledc_channel_config(&ledc_channel);
 }
 
-void set_stepper_frequency(int freq, float pitch) {
-
+void set_stepper_frequency(int freq, float pitch, int drn) {
+    
     ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_0, freq);
     ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 1024);
     ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
@@ -44,23 +44,37 @@ void set_stepper_frequency(int freq, float pitch) {
     } else {
         us = 1.00 / freq;
     }
-
-    int angle_to_move = pitch * STEPS_PER_REVOLUTION / 360;
-    for (int i = 0; i < angle_to_move; i++) {
-        if (phi >= 360.0) {
-            phi = 0;
+    if (drn == 1) {
+        int angle_to_move = pitch * STEPS_PER_REVOLUTION / 360;
+        for (int i = 0; i < angle_to_move; i++) {
+            if (phi >= 360.0) {
+                phi = 0;
+            }
+            phi += steps;
+            esp_rom_delay_us(us * 1000000);
         }
-        phi += steps;
-        esp_rom_delay_us(us * 1000000);
+        vTaskDelay(10);
+        ledc_stop(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 0);
+
+    } else {
+        int angle_to_move = pitch * STEPS_PER_REVOLUTION / 360;
+        for (int i = 0; i < angle_to_move; i++) {
+            if (phi <= 0.0) {
+                phi = 360;
+            }
+            phi -= steps;
+            esp_rom_delay_us(us * 1000000);
+        }
+        vTaskDelay(10);
+        ledc_stop(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 0);
     }
-    vTaskDelay(10);
-    ledc_stop(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 0);
+    printf("\nfreq: %d\n", freq);
 }
 
 float get_phi_angle() { return phi; }
 
 void set_stepper_direction(int direction) {
-    // Set the GPIO level for the DIR pin (0 or 1)
+    
     gpio_set_level(GPIO_DIR, direction);
 
     ESP_LOGI(TAG, "Stepper direction set to %d", direction);
@@ -69,7 +83,7 @@ void set_stepper_direction(int direction) {
 void stepper_idle() {
     ledc_stop(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 0);
 
-    // Set the direction pin to low
+ 
     gpio_set_level(GPIO_DIR, 0);
 
     ESP_LOGI(TAG, "Stepper motor set to idle state");
